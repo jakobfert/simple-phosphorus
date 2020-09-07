4c95fc8cec8636f9a7427ea817d93140a20605f3
Jakob Pferdmenges <JakobPferdmenges@gmx.de>
2020-09-07 16:16:30 +0200

    corrected save file for phosphorus

--- main.py
+++ main.py
@@ -476,7 +476,6 @@ if __name__ == '__main__':
         runs = int(sys.argv[4])
         water_or_phosphorus = str(sys.argv[5])  # 'water' or 'phosphorus'
 
-    dbname = 'results/MC_' + water_or_phosphorus + '_FF' + str(fastflow) + '_P' + str(prof)
     vgm_params_via_spotpy = True
 
     use_spotpy = True
@@ -489,15 +488,23 @@ if __name__ == '__main__':
                            irrigation=irr, profile=prof, flow_approach=fastflow, mode=water_or_phosphorus)
 
     if use_spotpy:
+        if water_or_phosphorus == 'water':
+            dbname = 'results/MC_water_FF' + str(fastflow) + '_P' + str(prof)
+        else:
+            if w_params_from_file:
+                index = int(sys.argv[6]) if len(sys.argv) >= 7 else 0
+                dbname = 'results/MC_phosphorus_FF' + str(fastflow) + '_P' + str(prof) + '_I' + str(index)
+            else:
+                dbname = 'results/MC_phosphorus_FF' + str(fastflow) + '_P' + str(prof) + '_test'
+
         # sampler = spotpy.algorithms.lhs(setup, parallel=parallel(), dbname=dbname, dbformat='csv')
         sampler = spotpy.algorithms.mc(setup, parallel=parallel(), dbname=dbname, dbformat='csv')
         sampler.sample(runs)
 
         eval_list = ModelInterface.evaluation(setup)
         results = sampler.getdata()
-        # spotpy.analyser.plot_parametertrace(results, fig_name='LHS_FF' + str(fastflow) + '_P' + str(
-        #     prof) + '_parameter_trace.png')
-        spotpy.analyser.plot_bestmodelrun(results, eval_list, fig_name='MC_FF' + str(fastflow) + '_P' + str(
-            prof) + '_best_model_run.png')
+
+        # spotpy.analyser.plot_parametertrace(results, fig_name=dbname + '_parameter_trace.png')
+        spotpy.analyser.plot_bestmodelrun(results, eval_list, fig_name=dbname + '_best_model_run.png')
     else:
         single_run = SingleRun(setup, water=w_params, phosphorus=p_params)
